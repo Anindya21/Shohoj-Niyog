db85afe284337fc87808a9658ba1518e3bce5331
Anindya Basak <39338592+Anindya21@users.noreply.github.com>
2025-10-08 23:47:46 +0600

    replaced previous model with quantized model

--- backend/logics/llm/model.py
+++ backend/logics/llm/model.py
@@ -5,9 +5,9 @@ import torch
 def load_llm():
     device = 0 if torch.cuda.is_available() else -1
 
-    tokenizer = AutoTokenizer.from_pretrained("context-labs/meta-llama-Llama-3.2-3B-Instruct-FP16")
+    tokenizer = AutoTokenizer.from_pretrained("unsloth/Llama-3.2-3B-Instruct-bnb-4bit")
     model = AutoModelForCausalLM.from_pretrained(
-            "context-labs/meta-llama-Llama-3.2-3B-Instruct-FP16",
+            "unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
             torch_dtype=torch.float16,
             device_map="auto" if device == 0 else "cpu",
             offload_folder="offload",
